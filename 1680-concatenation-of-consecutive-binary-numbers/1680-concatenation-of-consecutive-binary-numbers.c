0a8d4ff94d6b75a0cb695da3d30f921501b25895
abdellah el bekkali <84334734+ael-bekk@users.noreply.github.com>
2022-09-23 20:50:28 +0100

    Time: 94 ms (25.00%), Space: 5.6 MB (25.00%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.c
@@ -0,0 +1,21 @@
+
+int calcul_len(int n)
+{
+    int i = 0;
+
+    while (n)
+        i++, n /= 2;
+    return (i);
+}
+
+int concatenatedBinary(int n)
+{
+    int i = 1;
+    long long sum = 1;
+    
+    while (++i <= n)
+        sum <<= calcul_len(i),
+        sum += i,
+        sum %= 1000000007;
+    return (sum % 1000000007);
+}
