7a6ef5bd93fb95ba7956074aee2fdc686470bd86
abdellah el bekkali <84334734+ael-bekk@users.noreply.github.com>
2022-09-28 11:35:36 +0100

    Time: 7 ms (53.86%), Space: 9 MB (93.29%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -1,9 +1,10 @@
-int cmpfunc (const void * a, const void * b) { return ( *(int*)a - *(int*)b ); }
-
-bool canMakeArithmeticProgression(int* arr, int arrSize)
-{
-    qsort(arr, arrSize, sizeof(int), cmpfunc);
-    for (int i = 1; i < arrSize - 1; i++)
-        if (arr[1] - arr[0] != arr[i + 1] - arr[i]) return (0);
-    return (1);
-}
+class Solution {
+public:    
+    bool canMakeArithmeticProgression(vector<int>& arr)
+    {
+        sort(arr.begin(), arr.end());
+        for (int i = 1; i < arr.size() - 1; i++)
+            if (arr[1] - arr[0] != arr[i + 1] - arr[i]) return (0);
+        return (1);
+    }
+};
