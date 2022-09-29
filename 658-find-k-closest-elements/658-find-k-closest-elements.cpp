f93e1286cdf3bd22da773f173abf0166d65705cf
abdellah el bekkali <84334734+ael-bekk@users.noreply.github.com>
2022-09-29 11:39:49 +0100

    Time: 86 ms (64.66%), Space: 31.2 MB (86.01%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -4,14 +4,10 @@ public:
     {
         int size = arr.size();
         int l = 0, r = size - 1;
-        int ar[10000];
         vector<int> res (close);
-        
-        for (int i = 0; i < size; i++)
-            ar[i] = abs(arr[i] - target);
 
         while (l < r && r - l > close - 1)
-            if (ar[l] <= ar[r]) r--;
+            if (abs(arr[l] - target) <= abs(arr[r] - target)) r--;
             else l++;
 
         for (int i = l; i < l + close && i < size; i++)
