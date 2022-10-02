ba4f2eecd963924f0caf62874596551f57aa084a
abdellah el bekkali <84334734+ael-bekk@users.noreply.github.com>
2022-10-02 21:11:59 +0100

    Time: 8 ms (97.39%), Space: 6 MB (96.09%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int arr[31][1001];
-    
+    int mod = 1000000007;
+
     int Rolls(int n, int k, int target)
     {
         long long sum = 0;
 
-        if (target < 0 || n < 0 || (n == 0 && target != 0) || k * n < target)
-            return (0);
-        if (!target && !n)
-            return (1);
-        if (arr[n][target] != -1)
-            return (arr[n][target]);
+        if (target < 0 || n < 0 || (n == 0 && target != 0) || k * n < target) return (0);
+        if (!target && !n)          return (1);
+        if (arr[n][target] != -1)   return (arr[n][target]);
         for (int i = 1; i <= k; i++)
-        {
-            int s = Rolls(n - 1, k, target - i);
-            sum = (sum + s) % 1000000007;
-        }
-        arr[n][target] = sum;
-        return (sum);
+            sum = (sum + Rolls(n - 1, k, target - i)) % mod;
+        return (arr[n][target] = sum);
     }
     
     int numRollsToTarget(int n, int k, int target)
