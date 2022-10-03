d0c787bc08613939490a8cf0c709e5c1ef72daa9
abdellah el bekkali <84334734+ael-bekk@users.noreply.github.com>
2022-10-03 12:15:56 +0100

    Time: 291 ms (38.89%), Space: 14.9 MB (11.11%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.c
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.c
@@ -6,15 +6,12 @@ int minCost(char *colors, int *neededTime, int neededTimeSize)
     int tmp = -1;
 
     for (int i = 1; i < neededTimeSize; i++)
-    {
         if (colors[i] == colors[i - 1] && tmp == -1)
             time += (neededTime[i] < neededTime[i - 1]) ? neededTime[i] : neededTime[i - 1],
             tmp = (neededTime[i] > neededTime[i - 1]) ? i : i - 1;
         else if (colors[i] == colors[i - 1] && tmp != -1)
             time += (neededTime[i] < neededTime[tmp]) ? neededTime[i] : neededTime[tmp],
             tmp = (neededTime[i] > neededTime[tmp]) ? i : tmp;
-        else
-            tmp = -1;
-    }       
+        else tmp = -1;
     return (time);
 }
