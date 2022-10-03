cecc6cb702cb5c9a9fcec453566d92b2db035bd2
abdellah el bekkali <84334734+ael-bekk@users.noreply.github.com>
2022-10-03 18:24:30 +0100

    Time: 0 ms (100.00%), Space: 6 MB (92.26%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int numDecodings(string s)
     {
-        int res = 0, pre1 = 1, pre2 = 1, r = 1, t;
+        int res = 0, pre1 = 1, pre2 = 1, r = 1;
         for (int i = 1; i < s.length(); i++)
             if (s[i] == '0' && (s[i - 1] > '2' || s[i - 1] == '0'))
                 return (0);
-            else if ((s[i] < '7' && s[i - 1] < '3' && s[i - 1] != '0'  && s[i] != '0' && s[i + 1] != '0')
-                        || (s[i - 1] == '1'&& s[i] != '0' && s[i + 1] != '0'))
+            else if (((s[i] < '7' && s[i - 1] == '2') || s[i - 1] == '1') && s[i - 1] != '0' && s[i] != '0' && s[i + 1] != '0')
                 res++,
-                t = pre2,
                 pre2 += pre1,
-                pre1 = t;
+                pre1 = pre2 - pre1;
             else if (res)
                 r *= pre2,
                 res = 0,
