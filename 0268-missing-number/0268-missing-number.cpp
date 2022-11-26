7c91c2b6b200a50676a860bc6e6abb99514fb8e1
abdellah el bekkali <elbekkaliabdellah2001@gmail.com>
2022-11-26 21:27:19 +0100

    Time: 54 ms (34.95%), Space: 17.9 MB (66.04%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        long long res = nums.size() * (nums.size() + 1) / 2;
+        int res = nums.size() * (nums.size() + 1) / 2;
         for (auto x : nums)
             res -= x;
         return res;
