a5400030d10510326b69ca1d763c8d9d05d69978
abdellah el bekkali <elbekkaliabdellah2001@gmail.com>
2022-11-29 22:47:43 +0100

    Time: 0 ms (100.00%), Space: 6.5 MB (80.93%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int longestPalindrome(string str, int res = 0) {
+        int s[127] = {};
+        
+        for (auto x : str)
+            s[x]++;
+        
+        for (int i = 0; i < 127; i++)
+            res += s[i] - (s[i] % 2),
+            res += (s[i] % 2 && !(res % 2));
+            
+            
+        return (res);
+    }
+};
