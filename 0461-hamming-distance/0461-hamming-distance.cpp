b008dec30df54ab6553e7444bc9e85d76749b960
abdellah el bekkali <elbekkaliabdellah2001@gmail.com>
2022-11-29 13:18:57 +0100

    Time: 3 ms (38.88%), Space: 5.9 MB (27.50%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -4,7 +4,7 @@ public:
         int res = 0;
         
         while (x || y)
-            res += ((x % 2) != (y % 2)), x /= 2, y /= 2;
+            res += ((x % 2)  != (y % 2)), x /= 2, y /= 2;
         return (res);
     }
 };
