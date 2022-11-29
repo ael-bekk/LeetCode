7f59b515d872ffabb2a55c150555fc6b4a726c2c
abdellah el bekkali <elbekkaliabdellah2001@gmail.com>
2022-11-29 13:13:08 +0100

    Time: 0 ms (100.00%), Space: 6 MB (27.50%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int res = 0;
+        
+        while (x || y)
+            res += ((x % 2) != (y % 2)), x /= 2, y /= 2;
+        return (res);
+    }
+};
